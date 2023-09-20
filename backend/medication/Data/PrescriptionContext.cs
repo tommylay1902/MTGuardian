01da5ce321c30c862b6caef5c7a08d63f1a20075
tommylay1902 <tommylay.c@gmail.com>
2023-09-20 09:57:14 -0700

    database model

---
+++ backend/medication/Data/PrescriptionContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using medication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace medication.Data
+{
+	public class PrescriptionContext : DbContext
+
+	{
+		public DbSet<Prescription> Prescriptions { get; set; } = null!;
+
+        public PrescriptionContext(DbContextOptions<PrescriptionContext> options) : base(options)
+        {
+        }
+
+
+    }
+}
+
