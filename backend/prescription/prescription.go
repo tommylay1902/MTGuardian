030214658e1147a3962cd1fbb3c2b6a349fc9195
tommylay1902 <tommylay.c@gmail.com>
2023-10-16 16:15:54 -0700

    added cors

--- backend/prescription/prescription.go
+++ backend/prescription/prescription.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/tommylay1902/prescriptionmicro/api/dataaccess"
 	"github.com/tommylay1902/prescriptionmicro/api/handlers"
 	"github.com/tommylay1902/prescriptionmicro/api/routes"
@@ -18,6 +19,14 @@ func main() {
 	}()
 	//test 2
 	app := fiber.New()
+	// Initialize default config
+	app.Use(cors.New())
+
+	// Or extend your config for customization
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:3000",
+		AllowHeaders: "*",
+	}))
 
 	prescriptionDAO := dataaccess.InitalizePrescriptionDAO(db)
 	prescriptionService := services.InitalizePrescriptionService(prescriptionDAO)
--- frontend/mt-guardian/app/prescriptions/page.tsx
+++ frontend/mt-guardian/app/prescriptions/page.tsx
@@ -13,6 +13,7 @@ const PrescriptionPage = async () => {
   const res = await fetch("http://0.0.0.0:8000/api/v1/prescription", {
     cache: "no-cache",
   });
+
   const prescriptions: Prescription[] = await res.json();
 
   return <PrescriptionTableView prescriptions={prescriptions} />;
