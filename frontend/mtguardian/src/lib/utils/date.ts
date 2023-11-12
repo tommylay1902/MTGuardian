3adf130f34a0896743fa84f346516460f78519a9
tommylay1902 <tommylay.c@gmail.com>
2023-11-11 19:42:49 -0800

    fixed date bug

--- frontend/mtguardian/src/lib/utils/date.ts
+++ frontend/mtguardian/src/lib/utils/date.ts
@@ -1,11 +1,9 @@
+import { DateTime } from "luxon";
+
 export function convertStringISO8601ToShortDate(dateString: string | null) {
   if (dateString == null) return;
-  const date = new Date(dateString);
-  const formattedDate = date.toLocaleDateString("en-US", {
-    month: "2-digit",
-    day: "2-digit",
-    year: "numeric",
-  });
-
-  return formattedDate;
+  const parsedDate = DateTime.fromISO(dateString, { zone: "utc" }).toFormat(
+    "MM-dd-yyyy"
+  );
+  return parsedDate;
 }
