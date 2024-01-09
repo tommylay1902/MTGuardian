f47c592b637490dcab3b93a6375807339ab55798
tommylay1902 <tommylay.c@gmail.com>
2024-01-09 15:23:46 -0800

    updated gitignore for prescriptionhistory:

--- backend/prescriptionhistory/history.go
+++ backend/prescriptionhistory/history.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"github.com/gofiber/fiber"
+	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/tommylay1902/prescriptionhistory/internal/config"
 )
