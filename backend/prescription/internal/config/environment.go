790ce7c1a36c4e9ac7238e87cf8a1dbbc115cdb2
tommylay1902 <tommylay.c@gmail.com>
2024-01-23 16:05:53 -0800

    update to loading env

--- backend/prescription/internal/config/environment.go
+++ backend/prescription/internal/config/environment.go
@@ -8,7 +8,8 @@ import (
 )
 
 func SetupEnvironment() (string, string, string) {
-	err := godotenv.Load()
+
+	err := godotenv.Load(".env")
 	if err != nil {
 		log.Fatal("Error loading .env file", err)
 	}
